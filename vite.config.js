224d9e24683772374b5833aa75e86546f3d7b19d
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-08-28 05:27:46 -0700

    Refactor plugins array in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/VaniDaxi-frontend/",
 
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
 
   server: {
     port: 5173,
