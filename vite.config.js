2e1256890e6b4ce0f0e824be377303e6a69c723b
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-08-28 05:11:33 -0700

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,1 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "/SHORASHOPP-frontend/",
-  plugins: [react()],
-  server: { port: 5173 }
-});
+base: "/VaniDaxi-frontend/"
