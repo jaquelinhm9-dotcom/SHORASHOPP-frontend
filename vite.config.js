5ce41520d062af785af558ac4cb9ce7a5242f69f
jaquelinhm9-dotcom <jaquelinhm9@gmail.com>
2026-08-28 05:15:04 -0700

    Update Vite config to include React plugin and server port

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,14 @@
-base: "/VaniDaxi-frontend/"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/VaniDaxi-frontend/",
+
+  plugins: [
+    react(),
+  ],
+
+  server: {
+    port: 5173,
+  },
+});
